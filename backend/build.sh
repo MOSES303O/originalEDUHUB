450a8b2fdd3c1d8825ed468b9e83ec60cd68dc0e
MOSES OCHIENG <mosejck303@gmail.com>
2026-02-01 02:34:12 +0300

    test50

--- backend/build.sh
+++ backend/build.sh
@@ -4,12 +4,11 @@ set -o errexit  # Exit on any error
 echo "Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
-
 echo "Changing to Django project directory..."
-#cd EDUHUB  # <-- THIS IS THE KEY LINE
+cd EDUHUB  # <-- THIS IS THE KEY LINE
 
 echo "Collecting static files..."
-python manage.py collectstatic --no-input
+#python manage.py collectstatic --no-input
 
 echo "Running migrations..."
 python manage.py migrate
