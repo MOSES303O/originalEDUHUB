e9f1cddab27eb116adfe8df4a7afce3e209ee5b1
MOSES OCHIENG <mosejck303@gmail.com>
2026-02-01 01:10:17 +0300

    rest 47

--- backend/build.sh
+++ backend/build.sh
@@ -5,14 +5,23 @@ echo "Installing dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
 
+echo "Changing to Django project directory..."
+#cd EDUHUB  # <-- THIS IS THE KEY LINE
+
 echo "Collecting static files..."
-python EDUHUB/manage.py collectstatic --noinput
+python manage.py collectstatic --no-input
 
 echo "Running migrations..."
-python EDUHUB/manage.py migrate
+python manage.py migrate
 
-echo "Creating/updating superuser..."
-python EDUHUB/manage.py shell << EOF
+echo "Build complete!"
+
+#if [[$CREATE_SUPERUSER]];"
+#then
+#    python manage.py createsuperuser --no-input
+#fi
+# Create superuser using CORRECT app path
+python manage.py shell << EOF
 from apps.authentication.models import User
 import os
 
